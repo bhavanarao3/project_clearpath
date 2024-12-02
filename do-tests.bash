595819a81358b8b95aa1021fcaefb02d66295986
Ana <bhavana3@umd.edu>
2024-12-02 01:18:53 -0500

    Modified yml

--- do-tests.bash
+++ do-tests.bash
@@ -29,9 +29,9 @@ source install/setup.bash
 colcon test --event-handlers console_direct+
 
 # Generate coverage report if tests pass
-lcov --capture --directory build --output-file coverage.info
-lcov --remove coverage.info '/opt/*' --output-file temp1.info
-lcov --extract temp1.info '*/libs/debris/*' '*/test/*' --output-file filtered_coverage.info
+lcov --capture --directory build --output-file coverage.info --rc lcov_branch_coverage=1
+lcov --remove coverage.info '/opt/*' --output-file filtered_coverage.info
+lcov --extract filtered_coverage.info '*/libs/debris/*' '*/test/*' --output-file filtered_coverage.info
 genhtml filtered_coverage.info --output-directory coverage_report
 
 
